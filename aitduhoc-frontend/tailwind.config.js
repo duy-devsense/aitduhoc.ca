69a0170e85e3039522c317270da12f17ca2929d3
Ngoc Duy Anh Nguyen <ngocduyanhnguyen@Ngocs-MacBook-Pro.local>
2022-03-24 01:00:04 -0400

    more initial setups

--- aitduhoc-frontend/tailwind.config.js
+++ aitduhoc-frontend/tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        color1: "#F45A2B",
+        color2: "#f48c2b",
+        color3: "#f42b32",
+      },
+    },
   },
   plugins: [],
-}
+};
