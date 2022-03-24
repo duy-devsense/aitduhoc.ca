a62202930fcfb03707accae20d545d16ff24f025
Ngoc Duy Anh Nguyen <ngocduyanhnguyen@Ngocs-MacBook-Pro.local>
2022-03-24 01:02:43 -0400

    add hero section, feature section, footer for home.js

---
+++ aitduhoc-frontend/src/components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Features from "./feature-section-2by2";
+import HeroSection from "./hero-section";
+import Footer from "./footer";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Features />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
