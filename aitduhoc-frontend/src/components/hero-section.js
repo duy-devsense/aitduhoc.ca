9fe373bfbecf18b172e497962cacd5e899845415
Ngoc Duy Anh Nguyen <ngocduyanhnguyen@Ngocs-MacBook-Pro.local>
2022-03-24 20:50:00 -0400

    update hero image

--- aitduhoc-frontend/src/components/hero-section.js
+++ aitduhoc-frontend/src/components/hero-section.js
@@ -159,7 +159,7 @@ const HeroSection = () => {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          src="https://images.pexels.com/photos/2513993/pexels-photo-2513993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt=""
         />
       </div>
